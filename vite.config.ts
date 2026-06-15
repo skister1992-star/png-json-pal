092181cbffe5eceac553ba75c3236d92997c71bd
skister1992-star <s.kister1992@gmail.com>
2026-06-15 19:51:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  vite: {
+    server: {
+      allowedHosts: ["st-cs.sebastian-kister.de"]
+    }
+  },
+
   tanstackStart: {
-    // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
-    // nitro/vite builds from this
     server: { entry: "server" },
   },
 });
